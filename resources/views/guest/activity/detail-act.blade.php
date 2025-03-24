3dc20b297618ce5861862a8fd3e627b5315cce9e
Faberttt <kisjo.official0411@gmail.com>
2025-03-24 12:03:28 +0700

    update banner

--- resources/views/guest/activity/detail-act.blade.php
+++ resources/views/guest/activity/detail-act.blade.php
@@ -5,7 +5,7 @@
 <div class="container-fluid bg-breadcrumb" style="background: linear-gradient(rgb(7, 51, 7), rgba(0, 0, 0, 0.2)), url('{{ asset('assets/img/default_about.jpg') }}'); background-position: center; background-repeat: no-repeat; background-size: cover; padding: 60px 0;">
     <div class="container text-center py-5">
         <h4 class="text-white display-4 mb-4 wow fadeInDown" data-wow-delay="0.1s">{{ __('messages.company_activity') }}</h4>
-        <ol class="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
+        <ol class="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" dat-=0OK9a-wow-delay="0.3s">
             <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('messages.home') }}</a></li>
             <li class="breadcrumb-item active text-primary">{{ __('messages.detail_activity') }}</li>            
         </ol>    
